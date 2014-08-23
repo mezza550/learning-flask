14b48a4d758cb339e9cae47b7c01788d5c2f23c3
Eric <eric@eric3d.com>
2014-08-22 21:16:38 -0700

    testing db

---
+++ db_create.py
@@ -0,0 +1,13 @@
+from app import db
+from models import BlogPost
+
+# create database and the db tables
+db.create_all()
+
+# insert
+db.session.add(BlogPost("Good", "Im good."))
+db.session.add(BlogPost("Well", "Im well."))
+
+
+# commit the changes
+db.session.commit()
