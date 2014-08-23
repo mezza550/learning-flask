564915e824348e9148fc5794161b543ad5f5a620
Michael Herman <hermanmu@gmail.com>
2014-08-23 11:50:37 -0700

    updated spelling error

--- models.py
+++ models.py
@@ -1,16 +1,19 @@
 from app import db
 
+from sqlalchemy import ForeignKey
+
 
 class BlogPost(db.Model):
+
     __tablename__ = "posts"
-    
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
 
     def __init__(self, title, description):
-	self.title = title
-	self.description = description
- 
+        self.title = title
+        self.description = description
+
     def __repr__(self):
-	return '<title {}'.format(self.title)
+        return '<title {}'.format(self.title)
